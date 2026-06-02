25108a91c9a551c4ad5c51e7e767ffb087af6b4c
Pierre-Luc MAURICE <pl.maurice.fr@gmail.com>
2026-06-02 17:13:59 +0200

    Update B.py

--- B.py
+++ B.py
@@ -9,14 +9,12 @@ dico = dict(sorted(objets_velo.items(), key=lambda item: item[1][2], reverse=Tru
 print (dico)
 t_start = time()
 
-m_tot = 0
-util_tot = 0
+m_tot,util_tot = 0, 0
 sac={}
 for key,(a,b,c) in dico.items():
     if m_tot + a <= C :
         sac[key]=[a,b,c]
-        m_tot= round(m_tot + a,2)
-        util_tot= util_tot + b
+        m_tot, util_tot= round(m_tot + a,2), round(util_tot+b,2)
 
 t_fin = time()
 
