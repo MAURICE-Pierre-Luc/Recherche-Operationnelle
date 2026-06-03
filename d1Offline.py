75de312fd9d3bfbfaa339af46681f6182825aaee
DIOxygen3 <jean.jalet420@gmail.com>
2026-06-03 14:02:28 +0200

    Update d1Offline.py

--- d1Offline.py
+++ d1Offline.py
@@ -1,11 +1,9 @@
+from time import time
 import pandas as pd
 dm = pd.read_excel('donneesmarchandises.xlsx')
-print(dm['Longueur'])
-
-dm = pd.read_excel('donneesmarchandises.xlsx')
-tri=dm['Longueur'].sort_values().reset_index(drop=True)
+tri=dm['Longueur'].sort_values(ascending=False).reset_index(drop=True)
 print(tri)
-
+t_start = time()
 L_contenere = 11.583
 l_contenere = 2.294
 h_contenere = 2.569
@@ -14,8 +12,11 @@ nbr_wagons = 1
 
 for i in range(0, len(tri),1) :
     if contenere + tri[i] <= L_contenere :
-        contenere= contenere + tri[i]
+        contenere = contenere + tri[i]
     else :
         nbr_wagons+=1
-        contenere=0
+        contenere = 0 + tri[i]
+
+t_end = time()
 print("Nombre de wagons :", nbr_wagons)
+print(f'temps d\'execution : {t_end-t_start}')
